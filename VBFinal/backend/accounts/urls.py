83bd38ccaaacd7d3dd2bb5e1af4288458a01eefc
GechKibr <getachewkibr1995@gmail.com>
2026-01-21 13:40:12 +0000

    system usage implementations

--- VBFinal/backend/accounts/urls.py
+++ VBFinal/backend/accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 from .views import UserViewSet
+from .system_monitor import get_system_stats
 
 router = DefaultRouter()
 router.register(r'accounts', UserViewSet, basename='accounts')
@@ -10,6 +11,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('accounts/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('accounts/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('accounts/system/stats/', get_system_stats, name='system_stats'),
 ]
 
 
